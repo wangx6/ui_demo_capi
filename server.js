2c056853c4e5ad568ed524e38c8d23aa10f14e8a
Martin O'Sullivan <mosabcdef@yahoo.com>
2021-03-10 16:42:07 +0000

    Added POST Quote API

--- server.js
+++ server.js
@@ -7,11 +7,18 @@ const app = express();
 const port = 8080;
 
 app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.post('/quote', (req, res) => {
+    const {body}  = req;
+    console.log(body.firstName);
     res.json({ok: true});
 });
 
-app.listen(post, () => {
+app.listen(port, () => {
     console.log(`listening on port ${port}}`);
 });
